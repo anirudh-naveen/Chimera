714831a47d88f24a84fcbc8d9996d59742030be9
wangsop <sophia.riverfront@gmail.com>
2026-01-05 14:50:01 -0600

    boss fight mechanics
    the heart will now run away from the player with increasing franticness as it loses health

--- Chimera/Assets/Scripts/ChimeraParts/UndyingHeart.cs
+++ Chimera/Assets/Scripts/ChimeraParts/UndyingHeart.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class UndyingHeart : Creature
 {
     public Image img;
     private bool less200 = false;
     private bool less100 = false;
+    private float heartspeed = 5.0f;
+    private Vector2 moveDirection = Vector2.right;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +23,8 @@ public class UndyingHeart : Creature
         attack = 0;
         attackSpeed = 100;
         inTrigger = new List<Creature>();
+        less200 = false;
+        less100 = false;
     }
 
     // Update is called once per frame
@@ -29,21 +36,34 @@ public class UndyingHeart : Creature
             Die();
         } else if (this.CurrentHealth <= 200 && !less200)
         {
+            Debug.Log("heart under 200 health");
             MusicClass[] sfxplayer = UnityEngine.Object.FindObjectsByType<MusicClass>(FindObjectsSortMode.InstanceID);
             if (sfxplayer != null && sfxplayer.Length > 0)
             {
                 sfxplayer[sfxplayer.Length - 1].PlayMusic(sfxplayer[sfxplayer.Length-1].HeartTrack2);
             }
             less200 = true;
+            heartspeed = 5.0f;
         } else if (this.CurrentHealth <= 100 && !less100)
         {
+            Debug.Log("heart under 100 health");
             MusicClass[] sfxplayer = UnityEngine.Object.FindObjectsByType<MusicClass>(FindObjectsSortMode.InstanceID);
             if (sfxplayer != null && sfxplayer.Length > 0)
             {
                 sfxplayer[sfxplayer.Length - 1].PlayMusic(sfxplayer[sfxplayer.Length - 1].HeartTrack3);
             }
+            heartspeed = 10.0f;
             less100 = true;
         }
+        if (less200)
+        {
+            transform.Translate(moveDirection * heartspeed * Time.deltaTime);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveDirection = collision.gameObject.transform.position - transform.position;
+        moveDirection.Normalize();
     }
     new void Die()
     {
